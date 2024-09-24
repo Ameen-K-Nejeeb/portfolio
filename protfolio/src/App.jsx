4a1d607bdf80c4e453add83b057382301424df50
DUDEamee <ameenameen8701@gmail.com>
2024-09-24 17:25:12 +0530

    Create : my services created and some data imported to webpage

--- protfolio/src/App.jsx
+++ protfolio/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar/Navbar'
 import Hero from './components/Navbar/Hero/Hero'
 import About from './components/About/About'
+import Services from './components/Services/Services'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <About/>
+      <Services/>
     </div>
   )
 }
