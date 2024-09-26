47a7474fbd52321b573bb656cfe0f927ec63bddc
DUDEamee <ameenameen8701@gmail.com>
2024-09-26 15:31:13 +0530

    update : Navbar underline functionality applied

--- protfolio/src/components/Navbar/Navbar/Navbar.jsx
+++ protfolio/src/components/Navbar/Navbar/Navbar.jsx
@@ -6,18 +6,18 @@ import underline from '../../../assets/nav_underline.svg'
 const Navbar = () => {
 
 
-  const [menu,setMenu] = useState("work");
+  const [menu,setMenu] = useState("services");
 
 
   return (
     <div className='Navbar'>
       <img src={ameen} alt="" />
       <ul className="nav-menu">
-        <li><p>Home</p>{menu=="home"?<img src={underline} alt='' /> : <></>}</li>
-        <li><p>About</p>{menu=="about"?<img src={underline} alt='' /> : <></>}</li>
-        <li><p>Services</p>{menu=="services"?<img src={underline} alt='' /> : <></>}</li>
-        <li><p>Portfolio</p>{menu=="work"?<img src={underline} alt='' /> : <></>}</li>
-        <li><p>Contact</p>{menu=="contact"?<img src={underline} alt='' /> : <></>}</li>
+        <li><p onClick={()=>setMenu("home")}>Home</p>{menu=="home"?<img src={underline} alt='' /> : <></>}</li>
+        <li><p onClick={()=>setMenu("about")}>About</p>{menu=="about"?<img src={underline} alt='' /> : <></>}</li>
+        <li><p onClick={()=>setMenu("services")}>Services</p>{menu=="services"?<img src={underline} alt='' /> : <></>}</li>
+        <li><p onClick={()=>setMenu("work")}>Portfolio</p>{menu=="work"?<img src={underline} alt='' /> : <></>}</li>
+        <li><p onClick={()=>setMenu("contact")}>Contact</p>{menu=="contact"?<img src={underline} alt='' /> : <></>}</li>
       </ul>
 
 
