6c3433cd950d0a1f5d484d5ebf7538e9ea8df8a2
DUDEamee <ameenameen8701@gmail.com>
2024-09-26 15:19:51 +0530

    update : basic Navbar underLine

--- protfolio/src/components/Navbar/Navbar/Navbar.jsx
+++ protfolio/src/components/Navbar/Navbar/Navbar.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import ameen from '../../../assets/AMEENn.png'
+import underline from '../../../assets/nav_underline.svg'
 
 const Navbar = () => {
+
+
+  const [menu,setMenu] = useState("work");
+
+
   return (
     <div className='Navbar'>
       <img src={ameen} alt="" />
       <ul className="nav-menu">
-        <li>Home</li>
-        <li>About</li>
-        <li>Services</li>
-        <li>Portfolio</li>
-        <li>Contact</li>
+        <li><p>Home</p>{menu=="home"?<img src={underline} alt='' /> : <></>}</li>
+        <li><p>About</p>{menu=="about"?<img src={underline} alt='' /> : <></>}</li>
+        <li><p>Services</p>{menu=="services"?<img src={underline} alt='' /> : <></>}</li>
+        <li><p>Portfolio</p>{menu=="work"?<img src={underline} alt='' /> : <></>}</li>
+        <li><p>Contact</p>{menu=="contact"?<img src={underline} alt='' /> : <></>}</li>
       </ul>
 
 
