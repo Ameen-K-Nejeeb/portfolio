1c4737a11fe4c24ccc45089ffc335c817b0ae99a
DUDEamee <ameenameen8701@gmail.com>
2025-03-11 22:31:16 +0530

    update : project section ,show more and show less ,functionality is added

--- portfolio/src/components/Services/Services.jsx
+++ portfolio/src/components/Services/Services.jsx
@@ -1,39 +1,47 @@
-import React from 'react'
-import './Services.css'
-import theme from '../../assets/theme_pattern.svg'
-import Services_Data from '../../assets/services_data'
-import arrow_icon from '../../assets/arrow_icon.svg'
+import React, { useState } from 'react';
+import './Services.css';
+import theme from '../../assets/theme_pattern.svg';
+import Services_Data from '../../assets/services_data';
+import arrow_icon from '../../assets/arrow_icon.svg';
+import arrow from '../../assets/arrow_icon.svg';
 
 const Services = () => {
+  // State to control showing all services or just 6
+  const [showAll, setShowAll] = useState(false);
+
+  // Set how many services to show based on `showAll`
+  const displayedServices = showAll ? Services_Data : Services_Data.slice(0, 6);
+
   return (
-    <div id='services' className='services'>
+    <div id="services" className="services">
+      <div className="services-title">
+        <h1>My Projects</h1>
+        <img src={theme} alt="" />
+      </div>
 
-        <div className="services-title">
-            <h1>My Projects</h1>
-            <img src={theme} alt="" />
-        </div>
+      <div className="services-container">
+        {displayedServices.map((service, index) => (
+          <div key={index} className="services-format">
+            <h3>{service.s_no}</h3>
+            <h2>{service.s_name}</h2>
+            <p>{service.s_desc}</p>
+            <div className="services-readmore">
+              <p>Read More</p>
+              <img src={arrow_icon} alt="" />
+            </div>
+          </div>
+        ))}
+      </div>
 
-
-
-        <div className="services-container">
-            {Services_Data.map((service,index)=>{
-                return <div key={index} className="services-format">
-                    <h3>{service.s_no}</h3>
-                    <h2>{service.s_name}</h2>
-                    <p>{service.s_desc}</p>
-                    <div className="services-readmore">
-                        <p>Read More</p>
-                        <img src={arrow_icon} alt="" />
-                    </div>
-                </div>
-            })}
-        </div>
-
-
-
-
+      <div
+        className="myproject-showmore"
+        onClick={() => setShowAll(prev => !prev)} // Toggle state
+      >
+        <p>{showAll ? 'Show Less' : 'Show More'}</p>
+        <img src={arrow} alt="" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
